279d818becf554745243729ad508e039b4333599
Mitchel Felske <mitchel.felske@outlook.com>
2015-01-22 18:21:49 -0200

    gulp stylus css preprocessor

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,17 @@
 var gulp = require('gulp');
-var concat = require('gulp-concat');
-var sourcemaps = require('gulp-sourcemaps');
-var uglify = require('gulp-uglify');
-var ngAnnotate = require('gulp-ng-annotate');
+var fs = require('fs');
 
-gulp.task('js', function () {
-    gulp.src(['ng/module.js', 'ng/**/*.js'])
-        .pipe(sourcemaps.init())
-            .pipe(concat('app.js'))
-            .pipe(ngAnnotate())
-            .pipe(uglify())
-        .pipe(sourcemaps.write())
-        .pipe(gulp.dest('assets'));
-})
+fs.readdirSync(__dirname + '/gulp')
+    .forEach(function (task) {
+        require('./gulp/' + task);
+    })
 
 gulp.task('watch:js', ['js'], function () {
     gulp.watch('ng/**/*.js', ['js']);
 })
 
-gulp.task('brackets-default', ['watch:js']);
+gulp.task('watch:css', ['css'], function () {
+    gulp.watch('css/**/*.styl', ['css']);
+})
+
+gulp.task('brackets-default', ['watch:js', 'watch:css']);
